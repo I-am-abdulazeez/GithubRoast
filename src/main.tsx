4517c7f56c45aa94dfb0af440cc226ddf3455ecd
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-12-31 18:44:45 +0100

    chore: react-hot-toast component registration

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
+
+import { Toaster } from "react-hot-toast";
+
 import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
+        <Toaster toastOptions={{ position: "bottom-center" }} />
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
