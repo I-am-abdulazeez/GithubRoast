3df24912920fe60d1b0d2c7dbcdb377b22fbef11
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-12-31 23:22:48 +0100

    feat: My Github data

---
+++ src/data.ts
@@ -0,0 +1,15 @@
+export const AzeezData = {
+  message: 'Hello there! Welcome to my Github profile.',
+  status: 'success',
+
+  id: 38980212,
+  name: "AbdulAzeez Olanrewaju",
+  company: "startup: ibrid",
+  avatar_url: "https://avatars.githubusercontent.com/u/38980212?v=4",
+  bio: "Hello! My name is Olanrewaju AbdulAzeez Olamide and I am a front-end developer with a strong passion for creating modern, interactive web experiences.",
+  public_repos: 62,
+  followers: 57,
+  following: 91,
+  location: "Lagos, Nigeria",
+  created_at: "2018-05-04T11:53:31Z",
+}
