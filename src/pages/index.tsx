f523821feedb924aa4c64b112cc27bd4cf6dd5ca
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-12-31 18:50:14 +0100

    chore: Homepage (GitRoast) intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,58 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+import { Button } from "@nextui-org/button";
+import { useNavigate } from "react-router-dom";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { TypeAnimation } from "react-type-animation";
+
+import { title } from "@/components/primitives";
+
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
+  const navigate = useNavigate();
+
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <span className={title()}>Make&nbsp;</span>
-          <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-          <br />
-          <span className={title()}>
-            websites regardless of your design experience.
-          </span>
-          <div className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library.
-          </div>
-        </div>
+      <section className="flex flex-col items-center h-[90vh] justify-center gap-4 py-8 md:py-10">
+        <h2
+          className={title({
+            size: "md",
+            className: "font-bold text-center text-white",
+          })}
+        >
+          GitRoast 🔥🔥🔥
+        </h2>
+        <TypeAnimation
+          sequence={[
+            "What does your GitHub bio really say?",
+            1000,
+            "Is it a masterpiece or a disaster?",
+            2000,
+            "Let GitRoast give you the verdict.",
+            2000,
+            "Your bio, roasted and toasted.",
+            2000,
+          ]}
+          wrapper="span"
+          speed={75}
+          role="heading"
+          className="text-xl text-center md:text-2xl text-red-300"
+        />
 
-        <div className="flex gap-3">
-          <Link
-            isExternal
-            className={buttonStyles({
-              color: "primary",
-              radius: "full",
-              variant: "shadow",
-            })}
-            href={siteConfig.links.docs}
-          >
-            Documentation
-          </Link>
-          <Link
-            isExternal
-            className={buttonStyles({ variant: "bordered", radius: "full" })}
-            href={siteConfig.links.github}
-          >
-            <GithubIcon size={20} />
-            GitHub
-          </Link>
-        </div>
+        <p className="text-white text-2xl text-center max-w-4xl mt-3 mb-5 leading-relaxed">
+          GitRoast takes your GitHub bio and turns it into the ultimate roast.
+          Drop your username, and we'll turn your bio into a hot, crispy comedy
+          roast. Perfect for laughs, some self-reflection, or roasting your
+          fellow devs—your GitHub presence will never be the same! 🔥😂
+        </p>
 
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing{" "}
-              <Code color="primary">pages/index.tsx</Code>
-            </span>
-          </Snippet>
-        </div>
+        <Button
+          size="lg"
+          className="text-white font-semibold text-lg bg-red-500 hover:bg-red-600"
+          radius="sm"
+          onClick={() => navigate("/roastme")}
+        >
+          🔥 Turn Up the Heat!
+        </Button>
       </section>
     </DefaultLayout>
   );
