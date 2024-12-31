ab9af4466f0481654c074fae12b319f891512fb7
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-12-31 18:39:50 +0100

    chore: Added Roast page to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
+import RoastPage from "@/pages/roastme";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
+      <Route element={<RoastPage />} path="/roastme" />
     </Routes>
   );
 }
