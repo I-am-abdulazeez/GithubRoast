3ea33bb1a7c28c5a8b64d7f385e48a25fc85ec72
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-12-31 18:48:37 +0100

    utils: handleError

---
+++ src/helpers/handle-error.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+
+export const handleAPIError = (error: unknown) => {
+  const errorMessage =
+    axios.isAxiosError(error) && error.response?.data?.message
+      ? error.response?.data?.message
+      : "An unknown error occurred during the roasting process.";
+  console.error("Roasting error:", error);
+  toast.error(errorMessage);
+};
