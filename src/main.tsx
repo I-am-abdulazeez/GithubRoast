2bce6b1b9e09d292b229e75746e0f681988e66b1
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-12-31 23:23:16 +0100

    chore: toast duration

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
-        <Toaster toastOptions={{ position: "bottom-center" }} />
+        <Toaster toastOptions={{ position: "bottom-center", duration: 5000 }} />
         <App />
       </Provider>
     </BrowserRouter>
